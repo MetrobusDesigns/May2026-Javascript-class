9afdf4af9e255c717e0ff432fcbf234bf81c78b7
MerryPixel Studios <metrobus210@gmail.com>
2026-05-12 13:23:51 -0700

    changed formula error. Same result computed

--- unit1/jsintroassignment/distance.js
+++ unit1/jsintroassignment/distance.js
@@ -6,7 +6,7 @@ y2 = 8
 
 //  Distance Formula: d = √((x2 - x1)² + (y2 - y1)²) 
 
-let xDifference = x2 - y1; //ER: 2
+let xDifference = x2 - x1; //ER: 2
 let yDifference = y2 - y1; //ER: 2
 
 let xDiffSquared = Math.pow(xDifference, 2); // ER: 4
@@ -16,4 +16,5 @@ let yDiffSquared = Math.pow(yDifference, 2); // ER: 4
 console.log (yDifference); // ER: 4
 
 let distance = Math.sqrt(xDiffSquared+ yDiffSquared);
-console.log (distance); // ER - 2.8284
+console.log (`the distance between the two points is ${distance}`); // ER - 2.8284
+
