1ff3a11ce42533f25dd1f6239b22995840762e54
MerryPixel Studios <metrobus210@gmail.com>
2026-05-13 15:28:27 -0700

    moved operator to second prompt

--- unit1/playground/calculator.js
+++ unit1/playground/calculator.js
@@ -10,19 +10,20 @@ const prompt = require('prompt-sync')({sigint: true});
   } //loop runs as long as the input is invalid,
   let myPrompt1 = Number(input1); //converts the input to a number
 
+let validOperators = ["+", "*", "-", "/"];
+let myPrompt3 = prompt("Enter a valid operator symbol to add (+), subtract (-), multiplay (*), or divide (/) (+, -, *, /): ");
+// .include - Determines whether an array includes a certain element, returning true or false
+while (!validOperators.includes(myPrompt3)){  
+    //! — the "not" operator. It flips true to false and false to true.
+    myPrompt3 = prompt("Invalid entry. Enter a valid operator: (+, -, *, /)")
+}//loop runs as long as the input is invalid,
+
   let input2 = prompt("Enter your second number: ");
   while (isNaN(Number(input2))) {
       input2 = prompt(`Your input is invalid. Enter a valid number: `);
   } //loop runs as long as the input is invalid,
   let myPrompt2 = Number(input2); //converts the input to a number
 
-let validOperators = ["+", "*", "-", "/"];
-let myPrompt3 = prompt("Enter a valid operator symbol to add (+), subtract (-), multiplay (*), or divide (/) (+, -, *, /): ");
-// .include - Determines whether an array includes a certain element, returning true or false as appropriate.
-while (!validOperators.includes(myPrompt3)){  
-    //! — the "not" operator. It flips true to false and false to true.
-    myPrompt3 = prompt("Invalid entry. Enter a valid operator: (+, -, *, /)")
-}//loop runs as long as the input is invalid,
 
 let result; //result must be empty to provide the answer
 
@@ -36,6 +37,6 @@ if (myPrompt3 === "+") {
     result = myPrompt1 * myPrompt2;
 } else {
     result = "Invalid entries. Please retry your query again."; //in case anything else goes wrong, start over
-}
+};
 
 console.log(`${input1} ${myPrompt3} ${input2} =`, result);
