4fdfa69e645cd2cfa751b22024478e97ec1849fa
Metrobus <metrobus210@gmail.com>
2026-05-18 12:30:41 -0700

    start of project player tied

---
+++ unit1/lockbox-and-rps/rps.js
@@ -0,0 +1,11 @@
+const prompt= require ("prompt-sync")({sigint: true});
+
+console.log("Player1 - enter 1=rock, 2=paper, or 3=scissors");
+let player1 = prompt("> ");
+
+console.log("Player2 - enter rock, paper, or scissors");
+let player2 = prompt( "> ");
+
+if(player1 === player2) {
+    console.log("it's a tie!");
+} else if( player1 === "rock" && player2 !== "paper")
