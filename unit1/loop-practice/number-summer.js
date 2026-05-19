72b309a552daba6cd2a710dd06bec531d24f30fe
MerryPixel Studios <metrobus210@gmail.com>
2026-05-19 14:26:01 -0700

    making assignment sumer-summer

---
+++ unit1/loop-practice/number-summer.js
@@ -0,0 +1,21 @@
+"use strict";
+const prompt = require("prompt-sync")({ sigint: true });
+
+/* write a program that continually prompts the user for numbers greater than 0 until the user enters a 0 at which point the sum of all the inputted numbers is logged out.
+Example
+
+Enter some numbers (type 0 when complete):
+> 20
+> 10
+> 3.2
+> 0
+
+Those numbers sum to 33.2
+*/
+
+let num = -1; 
+let sum
+console.log('Enter some random numbers. When complete, enter 0 to add them')
+while (num !== 0){
+input
+}
