ed9c7f699eda5337a0430258f096b74ab96825ef
Metrobus <metrobus210@gmail.com>
2026-05-20 11:54:19 -0700

    doing exercise

---
+++ unit1/Array-Problems1/getindex3.js
@@ -0,0 +1,9 @@
+// given an array, print out the item at the 3rd index in the array
+// if there aren't enough items for there to be a 3rd index, print out the value at the last index instead
+// assume the array will have at least 1 item
+
+
+let arr =["Sand" , "Water" , "Beach" , "Umbrella" , "Waves" , "Ice Cream"] // Umbrella
+// let arr2 = 
+
+// if (arr.length )
---
+++ unit1/Array-Problems1/islonglist.js
@@ -0,0 +1,8 @@
+// given an array
+
+// if the array is at least 10 items long, print out a boolean true 
+
+// if not, print out false
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
