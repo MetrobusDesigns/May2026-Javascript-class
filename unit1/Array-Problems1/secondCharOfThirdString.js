ba784d83c92f2e10764b0657066c9c2e970568bf
MerryPixel Studios <metrobus210@gmail.com>
2026-05-20 12:27:14 -0700

    added programs

---
+++ unit1/Array-Problems1/secondCharOfThirdString.js
@@ -0,0 +1,11 @@
+let strArray = ["laptop", "phone", "Desktop" , "Mac" , "PC"];
+//this selects the second (0, 1, 2 ) in the array, which is Desktop
+let string3 = strArray [2];
+console.log(string3);
+
+let char2 = string3[1];
+console.log(char2);
+// we are selecting the second letter out of the array, or 'e' out of Desktop
+char2= strArray[2][1];
+console.log(char2);
+// we are selecting the second letter out of the array, or 'e' out of Desktop
