4d5033c5809f5395a08d455c4db1e8b17f35473c
Metrobus <metrobus210@gmail.com>
2026-05-20 07:36:34 -0700

    created number doubler

---
+++ unit1/loop-practice/number-doubler.js
@@ -0,0 +1,11 @@
+"use strict";
+const prompt = require("prompt-sync")({ sigint: true });
+
+
+let n = parseInt(prompt("Enter a number to double:"));
+ 
+while (n < 100) {
+    n = n * 2;
+    console.log(n);
+}
+ 
