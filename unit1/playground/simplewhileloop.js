02fe01a39ff3fb91debdb7f04a486869b6d91510
MerryPixel Studios <metrobus210@gmail.com>
2026-05-20 22:42:50 -0700

    built a simple while loop

---
+++ unit1/playground/simplewhileloop.js
@@ -0,0 +1,6 @@
+let number = -2
+while (number < 20){
+    number = number + 2
+    console.log(number)
+};
+console.log('Congratulations! You built a simple while loop that executes this statement when its complete.');
