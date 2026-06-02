258a8a03193d4872216715bf35bb41a6c5d96bb6
MerryPixel Studios <metrobus210@gmail.com>
2026-06-02 12:55:21 -0700

    finished problem

---
+++ unit2/object-arrays-assignment/endsiny.js
@@ -0,0 +1,9 @@
+/*
+
+Write a function called endsInY that takes in an array of strings and returns true if all strings end in "y"
+
+endsInY(["cloudy", "sunny","rainy"]) => true
+endsInY(["hi", "by","my"]) -> false
+endsInY(["yay", "YAY", "y"]) -> true
+
+*/
