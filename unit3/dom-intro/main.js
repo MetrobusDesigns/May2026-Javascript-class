e9dbc02b999d10b497e5c01b76531bf13f2ac0a3
Metrobus <metrobus210@gmail.com>
2026-06-04 14:05:40 -0700

    added new folder and dom manipulation sheet

---
+++ unit3/dom-intro/main.js
@@ -0,0 +1,6 @@
+let child1 = document.querySelector(".child-1");
+console.log(child1)
+
+child1.innerText = "New Text!"
+// child1.textContent = "New Text!"
+// child1.innerHTML = "<h2>New Text!</h2>";
