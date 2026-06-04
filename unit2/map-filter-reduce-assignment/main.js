6e856c5ce1bab6a50c7015675971183bf6e32d12
Metrobus <metrobus210@gmail.com>
2026-06-04 09:09:43 -0700

    commit changes to js files

--- unit2/map-filter-reduce-assignment/main.js
+++ unit2/map-filter-reduce-assignment/main.js
@@ -1,2 +1,18 @@
 // https://github.com/glf30/map-filter-reduce-assignment/tree/main
 // do the first 4 problems
+
+let names2 = ["Maya", "Christopher", "Jordanson", "Zoe", "Bob", "William"];
+
+const shortNamesOnly = (arr) => {
+    // filter
+    return arr.filter((name) => name.length < 7)
+}
+
+console.log(shortNamesOnly(names2))
+
+function isDName(isD) {
+    return isD[0].toUpperCase() ==="D"? true : false;
+}
+
+console.log(isDName("Tyler"));
+
