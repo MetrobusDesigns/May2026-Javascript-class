145f8c48a17c7f587c45c655b2ecec5cabfc506e
Metrobus <metrobus210@gmail.com>
2026-06-04 11:27:08 -0700

    add functions to calculate lowercase characters and total adult ages

---
+++ unit2/practice-problems/crossArray.js
@@ -0,0 +1,10 @@
+/*
+crossArray
+Write a function crossArray that takes in 2 parameters, both arrays of numbers. Return a result array that includes the number at index 0 in the first array, then the number at index 0 in the second array, then the number at index 1 in the first array, then the number at index 1 in the 2nd array and so on. Assume both arrays will be the same size and neither will be empty
+
+[5, 7, 14, 5] - arr1
+[6, 2, 22, 1] - arr2
+
+crossArray(arr1,arr2) -> [5,6,7,2,14,22,5,1]
+
+*/
