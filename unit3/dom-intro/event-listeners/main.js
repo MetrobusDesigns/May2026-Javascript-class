b6bf0742d316db3508e19ce8011042bd9a357f89
Metrobus <metrobus210@gmail.com>
2026-06-09 10:27:28 -0700

    added info to event listeners

--- unit3/dom-intro/event-listeners/main.js
+++ unit3/dom-intro/event-listeners/main.js
@@ -7,7 +7,8 @@ let displayNum = document.querySelector("#number");
 // buttons
 let plusOne = document.querySelector("#plus-one");
 let double = document.getElementById("double");
-let reset = document.querySelector("#reset")
+let reset = document.querySelector("#reset");
+let plusN = document.querySelector("#plus-n");
 //this creates an event listener "click"
 plusOne.addEventListener("click" , () => {
     //console.log("button.clicked!");
@@ -26,5 +27,30 @@ const resetValue = () => {
     value = 0;
     displayNum.innerText = value;
 };
-
+// when passing in other functions as callbacks, do NOT use ()!  We are not trying to call the function until the event occurs.  using () means we are trying to call the function immediately
+// resetValue NOT resetValue()
 reset.addEventListener("click" , resetValue);
+
+const addNToValue = (n) => {
+    value += n;
+    displayNum.innerText = value;
+};
+// when you have parameters, create a seperate function and call that function INSIDE your callback function!
+plusN.addEventListener("click", () =>{
+    addNToValue(15);
+});
+//when the mouse ENTERS the displayNum, it will change color!
+displayNum.addEventListener("mouseenter", () => {
+    displayNum.style.background = "red";
+    displayNum.style.color = "black";
+})
+//when the mouse LEAVES the displayNum, it will change color!
+displayNum.addEventListener("mouseleave", () => {
+    displayNum.style.background = "";
+    displayNum.style.color = "";
+})
+
+const subtractOne = () => {
+    value --;
+    displayNum.innerText = value;
+}
