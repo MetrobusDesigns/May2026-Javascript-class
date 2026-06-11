cd39ff14c30508f74fb047268195fff7fc7b2f70
MerryPixel Studios <metrobus210@gmail.com>
2026-06-11 00:28:22 -0700

    added CSS elements

--- unit3/form-assignment/script.js
+++ unit3/form-assignment/script.js
@@ -1,1 +1,10 @@
 console.log("testing");
+
+let input = document.getElementById("#user-input")
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+    window.prompt(`hello ${user-input}`)
+    alert(input.value)
+input.value = "";});
+
